2a49a24a7e95147152d4b27a134ff1f3df905f4e
Andrew Naoum <andy.naoum@gmail.com>
2013-02-28 00:51:46 +1100

    Added setup.py script

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='git-update-server',
+      version='0.1',
+      description='HTTP server to handle POST notifications of updated repositories on GitHub and BitBucket.',
+      author='Andrew Naoum',
+      author_email='andy.naoum@gmail.com',
+      url='https://github.com/anaoum/git-update-server',
+      packages=['git_update_server'],
+      scripts=['git-update-server']
+)
